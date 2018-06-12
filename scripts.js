cf591d2cf6307bb7616dc3a4a58fb0e0b5d88a95
Sameer99 <sameer.saptarshi>
2018-06-12 02:38:48 -0600

    Initialized Website with initial front page

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  $(".titleText").hide(0).delay(500).fadeIn(3000).queue(false)
+  $(".SameerPortrait").hide(0).delay(500).fadeIn(3000).queue(false)
+  $(".aboutText").hide(0).delay(2500).fadeIn(3000).queue(true)
+  $(".linkedin").hide(0).delay(4000).fadeIn(1500).queue(true)
+  $(".github").hide(0).delay(4500).fadeIn(1500).queue(true)
+  $(".gmail").hide(0).delay(5000).fadeIn(1500).queue(true)
+  $(".spotify").hide(0).delay(5500).fadeIn(1500).queue(true)
+
+
+   console.log("hello")
+});
